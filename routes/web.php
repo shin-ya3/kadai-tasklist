66406716e1b572b7c31896c394cb1afe17e0784c
jon <jonnojimublog@gmail.com>
2018-09-23 04:28:03 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
+/*
 Route::get('/', 'TasksController@index');
-
+*/
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
